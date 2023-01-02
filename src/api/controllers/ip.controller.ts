b2f30d148bb79efdbc440db066881b6f057074a4
hellskater <ksrinivasrao531@gmail.com>
2023-01-03 02:43:05 +0530

    added port 1338

--- src/api/controllers/ip.controller.ts
+++ src/api/controllers/ip.controller.ts
@@ -35,7 +35,21 @@ export class IpController {
         ssl_certificate_key /etc/letsencrypt/live/codenative.link/privkey.pem; # managed by Certbot
         include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
         ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
-    }`;
+    }
+    
+    server {
+      listen 1338 ssl;
+      server_name ${slug}.codenative.link;
+  
+      location / {
+          proxy_pass http://${ip}:1338;
+      }
+  
+      ssl_certificate /etc/letsencrypt/live/codenative.link/fullchain.pem; # managed by Certbot
+      ssl_certificate_key /etc/letsencrypt/live/codenative.link/privkey.pem; # managed by Certbot
+      include /etc/letsencrypt/options-ssl-nginx.conf; # managed by Certbot
+      ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem; # managed by Certbot
+  }`;
 
       fs.appendFileSync("/etc/nginx/conf.d/proxy.conf", config);
 
